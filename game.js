65aaef93c00df50d2d5ef0c0e862572fe6bf70e4
Drew Martin <drew.e.martin@gmail.com>
2014-07-14 19:06:13 -0400

    added circle object constructor

--- game.js
+++ game.js
@@ -11,8 +11,24 @@ function Circle(){
 	// since these are the starting positions neither can 
 	// be bigger than 450 since the size of the game area 
 	// is 500 and the max size of the circles is 50 
-	this.diameter 
-	this.speed
+	this.diameter = 30 + Math.random() * 20;
+	this.speed = 500 + Math.random() * 1500;
+	// speed refers to animation speed
+		this.render() = function(){
+			this.$me = $('<div class="circle"></div>')
+			// dollar sign is due to convention
+			// it respresents object that will
+			// be dom elements
+				.css('left', this.x)
+				.css('top', this.y)
+				.css('height', this.diameter)
+				.css('width', this.diameter);
+			$('#game').append(this.$me);
+		};
+	};
+	// render is not an "official" JQUERY method.
+	// in this instance render links this object 
+	// to an actual DOM element
 };
 // N.B. -> all of these items are located outside of the "document ready function"
 
